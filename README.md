eee374da932417f985de12152fe58d903da20fcf
Benzir Ahammed Shawon <benzir1116@gmail.com>
2026-09-12 17:26:43 +0600

    feat: initial repository baseline import

---
+++ README.md
@@ -0,0 +1,48 @@
+> [!IMPORTANT]
+> This repository is no longer actively developed (last release 2023-03-06).
+>
+> When I first created this fork of Idle Master in 2017/2018 I aimed to fix a bug plus extend some functionality and never expected it to live this long.
+> Sadly I have not been able to find the time to properly take care of it and I have decided to leave this project as-is and archive it. I hope you find some alternative software to fill the void.
+>
+> Thank you.
+
+[![Build status](https://ci.appveyor.com/api/projects/status/96wf12emnlbmo4sj?svg=true)](https://ci.appveyor.com/project/JonasNilson/idle-master-extended)
+![Github All Releases](https://img.shields.io/github/downloads/JonasNilson/idle_master_extended/total.svg)
+
+
+# 🚀 Idle Master Extended
+
+Get your [Steam Trading Cards](https://steamcommunity.com/tradingcards/) the quickest possible way.
+
+
+## ❓ But what is Idle Master Extended?
+
+🔧 This is a fork of the **discontinued** [Idle Master](https://github.com/jshackles/idle_master) project by [jshackles](https://github.com/jshackles) (also known for [Enhanced Steam](https://github.com/jshackles/Enhanced_Steam)):
+
+> This program will determine which of your Steam games still have Steam Trading Card drops remaining, and will go through each application to simulate you being “in-game” so that cards will drop. It will check periodically to see if the game you’re idling has card drops remaining. When only one drop remains, it will start checking more frequently. When the game you’re idling has no more cards, it’ll move on to the next game. When no more cards are available, the program will terminate. 
+> 
+> This application requires Steam to be open and for you to be logged in.  This program is now being developed exclusively for Microsoft Windows. Non-Windows versions are available in the [Python repository](https://github.com/jshackles/idle_master_py) but may be deprecated or feature incomplete.
+> 
+> Source: [`jshackles/idle_master`](https://github.com/jshackles/idle_master) 
+
+
+## 🔽 Download
+
+- https://github.com/JonasNilson/idle_master_extended/releases
+
+
+## 💭 [Idle Master Extended Wiki](https://github.com/JonasNilson/idle_master_extended/wiki)
+
+- 🧰 [Get started](https://github.com/JonasNilson/idle_master_extended/wiki/Get-started)
+- ✨ [Features](https://github.com/JonasNilson/idle_master_extended/wiki/Features)
+- 🔄 [FAQ](https://github.com/JonasNilson/idle_master_extended/wiki/FAQ)
+- 🔨 [Troubleshooting](https://github.com/JonasNilson/idle_master_extended/wiki/Troubleshooting-and-common-solutions)
+- 🎨 [Contribute](https://github.com/JonasNilson/idle_master_extended/wiki/Contribute)
+- 🎁 [Donate](https://github.com/JonasNilson/idle_master_extended/wiki/Donate)
+
+
+## Credits
+
+Idle Master was created by [jshackles](https://github.com/jshackles), based on the original code created by Stumpokapow.
+
+Idle Master was written in `C#` using `Steamworks.NET` and [`CSteamworks`](https://github.com/rlabrecque/CSteamworks), and using open source icons from [Open Iconic](https://github.com/iconic/open-iconic).
---
+++ Source/IdleMasterExtended/CookieClient.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using IdleMasterExtended.Properties;
+
+namespace IdleMasterExtended
+{
+    public class CookieClient : WebClient
+    {
+        internal CookieContainer Cookie = new CookieContainer();
+
+        internal Uri ResponseUri;
+
+        /// <summary>
+        /// Instantiates a new <c>CookieClient</c> object.
+        /// </summary>
+        public CookieClient()
+        {
+            Cookie = GenerateCookies();
+            Encoding = Encoding.UTF8;
+        }
+
+        /// <summary>
+        /// Method <c>GetHttpAsync</c> will asynchronosly use a HTTP GET request (via <c>WebClient</c>) to the provided parameter <c>url</c>
+        /// </summary>
+        /// <param name="url">URL to send a GET request to</param>
+        /// <param name="count">Number of retries to send a GET request</param>
+        /// <returns>The HTML content of the HTTP GET response</returns>
+        public static async Task<string> GetHttpAsync(string url, int count = 3)
+        {
+            while (true)
+            {
+                var client = new CookieClient();
+                var content = string.Empty;
+
+                try
+                {
+                    content = await client.DownloadStringTaskAsync(url);
+                }
+                catch (Exception ex)
+                {
+                    Logger.Exception(ex, "CookieClient -> GetHttpAsync, for url = " + url);
+                }
+
+                if (!string.IsNullOrWhiteSpace(content) || count == 0)
+                    return content;
+
+                count--;
+            }
+        }
+
+        /// <summary>
+        /// Method <c>IsLogined</c> will attempt to find out if the user has successfully logged in or not.
+        /// </summary>
+        /// <returns><c>true</c> if there was information available that confirms the login as successful, otherwise <c>false</c></returns>
+        public static async Task<bool> IsLogined()
+        {
+            var document = new HtmlDocument();
+
+            try
+            {
+                var response = await GetHttpAsync(Settings.Default.myProfileURL);
+                document.LoadHtml(response);
+            }
+            catch (Exception ex)
+            {
+                Logger.Exception(ex, "CookieClient -> IsLogined, for url = " + Settings.Default.myProfileURL);
+            }
+
+            // The 'notification_count' contains the Steam profile notifications count. Every logged in user should have it available.
+            var notificationCountNode = document.DocumentNode.SelectSingleNode("//span[@class='notification_count']");
+
+            if (notificationCountNode is null || string.IsNullOrEmpty(notificationCountNode.InnerHtml))
+            {
+                // The user has no notification count available --> not logged in properly.
+                ClearCookies();
+                return false;
+            } 
+            else
+            {
+                return true;
+            }
+        }
+
+        public static async Task<bool> RefreshLoginToken()
+        {
+            var document = new HtmlDocument();
+
+            var cookies = new CookieContainer();
+            var target = new Uri("https://steamcommunity.com");
+
+            cookies.Add(new Cookie("Cookie", "steamRefresh_steam=" + Settings.Default.steamLoginSecure) { Domain = target.Host });
+
+            try
+            {
+                var response = await GetHttpAsync("https://login.steampowered.com/jwt/refresh?redir=https%3A%2F%2Fsteamcommunity.com");
+                document.LoadHtml(response);
+                
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Logger.Exception(ex, "CookieClient -> RefreshToken, for url = " + Settings.Default.myProfileURL);
+            }
+
+            return false;
+        }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            var request = base.GetWebRequest(address);
+            
+            if (request is HttpWebRequest)
+                (request as HttpWebRequest).CookieContainer = Cookie;
+            
+            return request;
+        }
+
+        protected override WebResponse GetWebResponse(WebRequest request, System.IAsyncResult result)
+        {
+            try
+            {
+                var baseResponse = base.GetWebResponse(request);
+
+                var cookies = (baseResponse as HttpWebResponse).Cookies;
+
+                // Check, if cookie should be deleted. This means that sessionID is now invalid and user has to log in again.
+                // Maybe this shoud be done other way (authenticate exception), but because of shared settings and timers in frmMain...
+                Cookie loginCookie = cookies["steamLoginSecure"];
+                if (loginCookie != null && loginCookie.Value == "deleted")
+                {
+                    ClearCookies();
+                }
+
+                ResponseUri = baseResponse.ResponseUri;
+                
+                return baseResponse;
+            }
+            catch (Exception ex)
+            {
+                Logger.Exception(ex, "CookieClient -> WebResponse = " + request);
+            }
+            
+            return null;
+        }
+
+        private static void ClearCookies()
+        {
+            Settings.Default.sessionid = string.Empty;
+            Settings.Default.steamLogin = string.Empty;
+            Settings.Default.steamLoginSecure = string.Empty;
+            Settings.Default.steamparental = string.Empty;
+            Settings.Default.steamMachineAuth = string.Empty;
+            Settings.Default.steamRememberLogin = string.Empty;
+            Settings.Default.myProfileURL = string.Empty;
+
+            Settings.Default.Save();
+        }
+
+        private static CookieContainer GenerateCookies()
+        {
+            var cookies = new CookieContainer();
+            var target = new Uri("https://steamcommunity.com");
+
+            cookies.Add(new Cookie("sessionid", Settings.Default.sessionid) { Domain = target.Host });
+            cookies.Add(new Cookie("steamLoginSecure", Settings.Default.steamLoginSecure) { Domain = target.Host });
+            cookies.Add(new Cookie("steamparental", Settings.Default.steamparental) { Domain = target.Host });
+            cookies.Add(new Cookie("steamRememberLogin", Settings.Default.steamRememberLogin) { Domain = target.Host });
+            cookies.Add(new Cookie(GetSteamMachineAuthCookieName(), Settings.Default.steamMachineAuth) { Domain = target.Host });
+
+            return cookies;
+        }
+
+        private static string GetSteamMachineAuthCookieName()
+        {
+            if (Settings.Default.steamLoginSecure != null && Settings.Default.steamLoginSecure.Length > 17)
+                return string.Format("steamMachineAuth{0}", Settings.Default.steamLoginSecure.Substring(0, 17));
+
+            return "steamMachineAuth";
+        }
+    }
+}
